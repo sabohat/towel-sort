4bbc9f88885034693e0dd34ddd8fcbbb29805308
Sabokhat Kalandarova <sabohat2802@gmail.com>
2021-03-11 20:02:10 +0500

    docs: added description to the code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-
-// You should implement your task here.
+/* We go throught every array and add its contents to our new array with REST operator (...) 
+Notice: if array in the matrix has an odd index, it should be reversed before pushing into new array*/
 
 module.exports = function towelSort (matrix) {
 
+  //return empty array if the input is invalid
   if (matrix == undefined || matrix.length == 0){
     return []
   }
-// function towelSort (matrix) {
-  let a = []
+  
+  //declare a new array to store values later on
+  let newArr= []
   for (let arr of matrix){
 
     index = matrix.indexOf(arr)
-
+    //index - even, then add contents of arr directly
     if (index % 2 == 0){
-      a.push(...arr)
+      newArr.push(...arr)
     }
+    //index - odd, then add contents of arr after reversing
     else{
       arr = arr.reverse()
-      a.push(...arr)
+      newArr.push(...arr)
     }
-    
-    console.log(arr)
-    
   }
-  console.log(a)
-  return a;
+  //our shiny new array here, Yayy!
+  return newArr;
 }
-
-// towelSort([
-//  [ 1, 2, 3 ],
-//  [ 4, 5, 6 ],
-//  [ 7, 8, 9 ],
-// ])
